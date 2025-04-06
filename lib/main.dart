b38cea77d167a59d6fd837cf9074bb4ff4062297
Arsim_Ajvazi <arsimajvazi420@gmail.com>
2025-04-06 05:33:02 +0000

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,91 +1,253 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(const CampusInfoSystemApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  static const String _title = 'Flutter Stateful Clicker Counter';
-  // This widget is the root of your application.
+class CampusInfoSystemApp extends StatelessWidget {
+  const CampusInfoSystemApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: _title,
+      title: 'Campus Information System',
       theme: ThemeData(
-        // useMaterial3: false,
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      debugShowCheckedModeBanner: false,
+      home: const LoginPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-  // This class is the configuration for the state.
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _LoginPageState createState() => _LoginPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _LoginPageState extends State<LoginPage> {
+  bool _passwordVisible = false;
+  final TextEditingController _emailController =
+      TextEditingController(text: "b30724109@std.izu.edu.tr");
+  final TextEditingController _passwordController =
+      TextEditingController(text: "••••••••");
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: const Text('Flutter Demo Click Counter'),
-      ),
+      backgroundColor: Colors.white,
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        child: SingleChildScrollView(
+          child: Container(
+            constraints: const BoxConstraints(maxWidth: 600),
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // University Logo
+                Image.network(
+                  'https://www.izu.edu.tr/images/default-source/logo-galeri/1.jpg?sfvrsn=18698650_2',
+                  height: 80,
+                  fit: BoxFit.contain,
+                ),
+                const SizedBox(height: 10),
+
+                // Campus Information System Text
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.school, color: Colors.grey[700], size: 22),
+                    const SizedBox(width: 5),
+                    Text(
+                      'Campus Information System',
+                      style: TextStyle(
+                        fontSize: 22,
+                        color: Colors.grey[700],
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+
+                const SizedBox(height: 10),
+
+                // Divider Line
+                Container(
+                  height: 2,
+                  color: Colors.blue[700],
+                ),
+
+                // White Space
+                Container(
+                  color: Colors.white,
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      const Text(
+                        'Enter Your Login Information',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Color(0xFF555555),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+
+                      // Email Input
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey[400]!),
+                        ),
+                        child: TextField(
+                          controller: _emailController,
+                          decoration: InputDecoration(
+                            prefixIcon:
+                                Icon(Icons.person, color: Colors.grey[400]),
+                            border: InputBorder.none,
+                            contentPadding:
+                                const EdgeInsets.symmetric(vertical: 12),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+
+                      // Password Input
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey[400]!),
+                        ),
+                        child: TextField(
+                          controller: _passwordController,
+                          obscureText: !_passwordVisible,
+                          decoration: InputDecoration(
+                            prefixIcon:
+                                Icon(Icons.lock, color: Colors.grey[400]),
+                            suffixIcon: IconButton(
+                              icon: Icon(
+                                _passwordVisible
+                                    ? Icons.visibility
+                                    : Icons.visibility_off,
+                                color: Colors.grey[400],
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  _passwordVisible = !_passwordVisible;
+                                });
+                              },
+                            ),
+                            border: InputBorder.none,
+                            contentPadding:
+                                const EdgeInsets.symmetric(vertical: 12),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+
+                      // Login Button
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.blue[700],
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 20, vertical: 12),
+                            ),
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                const Text(
+                                  'Login - Sign In',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                const SizedBox(width: 5),
+                                const Icon(Icons.arrow_forward,
+                                    size: 16, color: Colors.white),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+
+                // Create Account Section
+                Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    border: Border(
+                      top: BorderSide(color: Colors.grey[300]!),
+                      bottom: BorderSide(color: Colors.grey[300]!),
+                    ),
+                  ),
+                  padding: const EdgeInsets.symmetric(vertical: 15),
+                  child: Column(
+                    children: [
+                      TextButton(
+                        onPressed: () {},
+                        child: const Text(
+                          'Create account',
+                          style: TextStyle(color: Colors.black87),
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: const Text(
+                          'I forgot my password',
+                          style: TextStyle(color: Colors.black87),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                // Graduate Student Login
+                Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(color: Colors.grey[300]!),
+                    ),
+                  ),
+                  padding: const EdgeInsets.symmetric(vertical: 15),
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Graduate student login',
+                      style: TextStyle(color: Colors.black87),
+                    ),
+                  ),
+                ),
+
+                // Course Enrollment Button
+                Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.symmetric(vertical: 15),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF3498db),
+                      padding: const EdgeInsets.symmetric(vertical: 15),
+                    ),
+                    child: const Column(
+                      children: [
+                        Text(
+                          'Click for course enrollment and advisor approval',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        Text(
+                          '.',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: const TextStyle(fontSize: 25),
-            ),
-          ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
